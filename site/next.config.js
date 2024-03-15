549d835b2f915361af31b9b61dbd4ff750588557
Pietro <47533868+subastation@users.noreply.github.com>
2024-03-15 06:54:15 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'FreeFlow Registry',
+    description: 'Custom FreeFlow registry',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/subastation/workspaces_registry/',
+    contactUrl: 'https://github.com/subastation/workspaces_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/workspaces_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
