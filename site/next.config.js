760e153d45cc3d368c8214c39eaed7b34a1ed292
Pietro <47533868+subastation@users.noreply.github.com>
2024-03-19 06:07:27 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   env: {
     name: 'FreeFlow Registry',
     description: 'Custom FreeFlow registry',
-    icon: '/img/logo.svg',
-    listUrl: 'https://github.com/subastation/workspaces_registry/',
+    icon: 'https://xflowx.com/images/hero.jpg',
+    listUrl: 'https://subastation.github.io/workspaces_registry/',
     contactUrl: 'https://github.com/subastation/workspaces_registry/issues',
   },
   reactStrictMode: true,
